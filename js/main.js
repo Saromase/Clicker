240249f0ee1934be5214f8e4e4d4aa80081f5e7d
saromase <seite.romain@hotmail.fr>
2017-07-20 23:11:53 +0200

    add design

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* Variable */
+var score = 0; var zombykill = 0; var multiplier = 1;
+/* Execute */
+
+/* Display */
+function displayAll(id, variable) {
+    return document.getElementById(id).innerHTML = variable;
+}
