b760bd64b48fe8ba535e4fa35fddcf67e8fe6be9
Romain <seite.romain@yahoo.com>
2017-07-21 17:13:42 +0200

    AddWeapon

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
 /* Variable */
-var score = 0;
+var score = 500;
 var zombykill = 0;
 var multiplier = 1;
+
+/* Weapons */
+
+// Brush
+var lvlBrush = 0;
+var priceBrush = 200;
+var killBrush = 0;
+
+function addBrush() {
+    if ((score >= priceBrush ) && (lvlBrush == 0)){
+        lvlBrush++;
+        score = score - priceBrush;
+        var message = "Vous avez augmenté le niveau de votre Pinceau";
+        return display("score", score) + display("lvlbrush", lvlBrush) + display("pricebrush", priceBrush) + display("message", message);
+    } else {
+        if (score >= priceBrush) {
+        lvlBrush++;
+        score = score - priceBrush;
+        var message = "Vous avez augmenté le niveau de votre Pinceau";
+        return display("score", score) + display("lvlbrush", lvlBrush) + display("pricebrush", priceBrush) + display("message", message);
+    } else {
+        var message = "Vous n'avez pas assez de rage pour augmenter votre arme il vous en faut : " + priceBrush;
+        return display("message", message);
+    }
+    }
+    
+}
+
 
 /* Execute */
 function hitZomby() {
@@ -15,13 +43,15 @@ function upgradeMultiplier() {
     if (score >= priceMultiplier) {
         multiplier++;
         score = score - priceMultiplier;
-        var message = "Vous avez augmenter votre multiplicateur";
+        var message = "Vous avez augmenté votre multiplicateur";
         return display("score", score) + display("multiplier", multiplier) + display("message", message);
     } else {
         var message = "Vous n'avez pas assez de rage pour multiplier il vous en faut : " + priceMultiplier;
         return display("message", message);
     }
 }
+
+
 
 /* Display */
 function display(id, variable) {
