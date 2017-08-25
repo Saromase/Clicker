88c2509cf8d14a8c8445d7fddc2640b737908064
Romain <seite.romain@yahoo.com>
2017-08-25 12:13:28 +0200

    add autoclicker

--- ClickerObjet/js/main.js
+++ ClickerObjet/js/main.js
@@ -24,7 +24,7 @@ var Clicker = {
             alert("pas assez de sous");
         }
     },
-    setNewWeapon: function (price) {
+    setNewWeapon: function (price, add) {
         if (price <= this.rage) {
             this.rage -= price;
             this.score -= price;
@@ -32,16 +32,18 @@ var Clicker = {
             weapon.lvl = 1;
             weapon.kill = 0;
             weapon.price = price;
+            weapon.update = add;
             Clicker.displayScore();
             return weapon;
         } else {
             alert("test");
         }
     },
-    addWeapon: function (variable, price) {
+    addWeapon: function (variable, price, add) {
         if (variable.price < this.rage) {
             variable.lvl++;
             this.rage -= variable.price;
+            variable.update += add;
             variable.price = Math.floor(variable.price * 1.5);
             Clicker.displayScore();
             return variable;
@@ -49,10 +51,16 @@ var Clicker = {
             alert("pas de tune");
             return variable;
         }
+    },
+    addRage: function (weapon) {
+        Clicker.score += weapon.update;
+        Clicker.rage += weapon.update;
+        Clicker.displayScore();
     }
 };
 
 var brush;
+var bat;
 $(document).ready(function () {
     $("#target").click(function () {
         Clicker.hit();
@@ -61,13 +69,33 @@ $(document).ready(function () {
         Clicker.addMultiplier();
     });
 
-    $("#addWeapon0").click(function () {
+    $("#weapon0").click(function () {
         if (typeof brush === "undefined") {
-            brush = Clicker.setNewWeapon(100);
+            brush = Clicker.setNewWeapon(100, 10);
             console.log(brush);
+            setInterval(function () {
+                Clicker.score += brush.update;
+                Clicker.rage += brush.update;
+                Clicker.displayScore();
+            }, 1000);
         } else {
-            brush = Clicker.addWeapon(brush, brush.price);
+            brush = Clicker.addWeapon(brush, brush.price, 10);
             console.log(brush);
+        }
+    });
+    $("#weapon1").click(function () {
+        if (typeof bat === "undefined") {
+            bat = Clicker.setNewWeapon(250, 50);
+            console.log(bat);
+            setInterval(function () {
+                Clicker.score += (bat.update * Clicker.multiplier);
+                Clicker.rage += (bat.update * Clicker.multiplier);
+                Clicker.displayScore();
+            }, 1000);
+
+        } else {
+            bat = Clicker.addWeapon(bat, bat.price, 50);
+            console.log(bat);
         }
     });
 });
